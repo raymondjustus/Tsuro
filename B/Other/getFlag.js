e007978875802d44bcf4f2a952296974ab05b0b9
Josh Pensky <joshuapensky@gmail.com>
2019-09-21 13:33:16 -0400

    updating error message to use flag type constants

--- B/Other/getFlag.js
+++ B/Other/getFlag.js
@@ -13,12 +13,12 @@ const { FLAG_INDEX, FLAG_TYPES } = require('./constants');
 const getFlag = (args = []) => {
   const hasFlag = args.length > FLAG_INDEX;
   if (!hasFlag) {
-    throw 'You must include an argument -up or -down flag.';
+    throw `You must include an argument ${FLAG_TYPES.UP} or ${FLAG_TYPES.DOWN} flag.`;
   }
 
   const flag = args[FLAG_INDEX];
   if (flag !== FLAG_TYPES.DOWN && flag !== FLAG_TYPES.UP) {
-    throw 'Your provided flag must be either -up or -down.';
+    throw `Your provided flag must be either ${FLAG_TYPES.UP} or ${FLAG_TYPES.DOWN}.`;
   }
 
   return flag;
