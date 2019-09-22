456077412d2ffab483a00f5e3f7efd673681e860
Josh Pensky <joshuapensky@gmail.com>
2019-09-22 00:08:05 -0400

    adds sortItems function

--- B/Other/index.js
+++ B/Other/index.js
@@ -1,15 +1,22 @@
 const Parser = require('./Parser');
-// const getFlag = require('./getFlag');
+const getFlag = require('./getFlag');
 const getInput = require('./getInput');
+const sortItems = require('./sortItems');
 
+/**
+ * Main script function. Reads input from stdin, parses it
+ * into valid JSON, sorts it based on a given flag, and
+ * logs it to stdout.
+ */
 const main = () => {
   try {
-    // const flag = getFlag(process.argv);
+    const flag = getFlag(process.argv);
     getInput().then(input => {
-      console.log('---------');
       const parser = new Parser();
       const items = parser.parse(input);
-      console.log(items);
+      const sortedItems = sortItems(items, flag);
+      console.log('------------------------------');
+      sortedItems.forEach(item => console.log(JSON.stringify(item)));
     });
   } catch (error) {
     console.log(`ERROR: ${error}`);
