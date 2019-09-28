1d85031d29ab3faf65748c7b8a158d5652769274
Raymond Namar <namar.ra@husky.neu.edu>
2019-09-28 14:08:14 -0400

    cleaning and becoming a factory worker

--- C/Other/1/server.js
+++ C/Other/1/server.js
@@ -1,15 +1,19 @@
 const { createServer } = require('net');
 const Ref = require('../../../B/Other/Ref');
 
+const onData = inputRef => chunk => {
+  const text = chunk.toString().trim();
+  inputRef.set(input => `${input}${text} `);
+};
+
+const onClose = inputRef => () => {
+  console.log(inputRef.get());
+};
+
 const onConnection = socket => {
-  const input = new Ref();
-  socket.on('data', chunk => {
-    const text = chunk.toString().trim();
-    input.set(input => `${input}${text} `);
-  });
-  socket.on('end', () => {
-    console.log(input.get());
-  });
+  const inputRef = new Ref();
+  socket.on('data', onData(inputRef));
+  socket.on('close', onClose(inputRef));
 };
 
 const server = createServer(onConnection);
