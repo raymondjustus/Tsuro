75f9b470acc20630c5ae24d7f6edae3b7b1782a5
William Epstein <epstein.w@husky.neu.edu>
2019-10-15 22:06:55 -0400

    close to working, stack overflow during filtering over last 12 items

--- Tsuro/1/allTiles.js
+++ Tsuro/1/allTiles.js
@@ -16,6 +16,31 @@ const tileRepetitionCheck = (tileList, tile) => {
 };
 
 /**
+ * Checks to see if the given tile is in the tile list, does not check for rotation equality
+ * @param {Tile[]} tileList
+ * @param {Tile Object} tile
+ */
+const simpleRepetitionCheck = (tileList, tile) => {
+  for (let i = 0; i < tileList.length; i++) {
+    if (tile.checkTileEquality(tileList[i])) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// const smartFilter = tiles => {
+//   let filteredList = [];
+//   tiles.array.forEach(item => {
+//     if (simpleRepetitionCheck(filteredList, item)) {
+//       filteredList.push(item);
+//     }
+//   });
+
+//   return filteredList;
+// };
+
+/**
  * Creates one version of each 35 possible tiles for the game Tsuro
  */
 const allTiles = () => {
@@ -28,21 +53,24 @@ const allTiles = () => {
   let five = [DIRECTIONS.SOUTH, 1];
   let six = [DIRECTIONS.WEST, 0];
   let seven = [DIRECTIONS.WEST, 1];
-  let portOptions = [one, two, three, four, five, six, seven];
+
   let count = 0;
 
   for (let i = 0; i < 7; i++) {
-    // console.log("got here 1");
+    let portOptions = [one, two, three, four, five, six, seven];
     let startFirst = zero;
-    let endFirst = portOptions.splice(0, 1);
+    // console.log("got here 1");
+    console.log("removed " + startFirst);
+    let endFirst = portOptions.splice(i, 1).shift();
     for (let j = 0; j < 5; j++) {
       // console.log("got here 2");
       let startSecond = portOptions.shift();
-      let endSecond = portOptions.shift();
+      let endSecond = portOptions.splice(j, 1).shift();
       for (let k = 0; k < 3; k++) {
         // console.log("got here 3");
+        console.log("Len: " + portOptions.length);
         let startThird = portOptions.shift();
-        let endThird = portOptions.shift();
+        let endThird = portOptions.splice(k, 1).shift();
         let startFourth = portOptions.shift();
         let endFourth = portOptions.shift();
         // console.log("got here 4");
@@ -56,81 +84,52 @@ const allTiles = () => {
         let temporary = new Tile();
         temporary.addPaths(layout);
 
-        if (tileRepetitionCheck(foundTiles, temporary)) {
-          foundTiles.push(temporary);
-        }
+        // if (tileRepetitionCheck(foundTiles, temporary)) {
+        foundTiles.push(temporary);
+        //   console.log("Found another: " + foundTiles.length);
+        // }
 
-        portOptions.push(startThird);
-        portOptions.push(endThird);
-        portOptions.push(startFourth);
-        portOptions.push(endFourth);
+        portOptions.unshift(endFourth);
+        portOptions.unshift(startFourth);
+        portOptions.unshift(endThird);
+        portOptions.unshift(startThird);
       }
-      portOptions.push(startSecond);
-      portOptions.push(endSecond);
+      portOptions.unshift(endSecond);
+      portOptions.unshift(startSecond);
     }
-    portOptions.push(endFirst);
+    portOptions.unshift(endFirst);
+    portOptions.unshift(startFirst);
+    console.log("JUST CHECKING: " + JSON.stringify(portOptions));
   }
 
-  console.log("LENGTH: " + foundTiles.length);
-
-  /*
-    list of made tiles M
-
-    for loop {
-        pick the first pair
-        for loop {
-            pick the second pair
-            for loop {
-                pick the third pair
-                whatever is left is the last
-                
-                tile T
-                for loop over M {
-                    check if t == m is true, 
-                    if for all m in M t == m if false, add T to m
-                }
-
-
-
-            }
-        }
+  let finalList = [];
+  console.log(foundTiles.length);
+  foundTiles.forEach(item => {
+    if (simpleRepetitionCheck(finalList, item)) {
+      finalList.push(item);
     }
+  });
+  console.log("LENGTH: " + finalList.length);
+  let lastList = [];
+  while (finalList.length > 0) {
+    let item = finalList.pop();
+    if (tileRepetitionCheck(lastList, item)) {
+      lastList.push(item);
+      console.log(lastList.length + " -- " + JSON.stringify(item.paths));
+    }
+    if (lastList.length >= 34) {
+      console.log("Nearing end: " + finalList.length);
+      break;
+    }
+  }
 
-
-
-
-
-
-  */
-
-  const t = new Tile();
-  const a = new Tile();
-  var paths = [
-    [[DIRECTIONS.NORTH, 0], [DIRECTIONS.NORTH, 1]],
-    [[DIRECTIONS.WEST, 1], [DIRECTIONS.SOUTH, 0]],
-    [[DIRECTIONS.EAST, 0], [DIRECTIONS.SOUTH, 1]],
-    [[DIRECTIONS.WEST, 0], [DIRECTIONS.EAST, 1]]
-  ];
-
-  var easyPath = [
-    [[DIRECTIONS.NORTH, 0], [DIRECTIONS.NORTH, 1]],
-    [[DIRECTIONS.WEST, 1], [DIRECTIONS.SOUTH, 1]],
-    [[DIRECTIONS.EAST, 0], [DIRECTIONS.SOUTH, 0]],
-    [[DIRECTIONS.WEST, 0], [DIRECTIONS.EAST, 1]]
-  ];
-
-  t.addPaths(easyPath);
-  a.addPaths(paths);
-  //   console.log(a.paths);
-  //   a.rotate(1);
-  //   console.log("EQUAL? " + t.tileEquality(a));
-  //   t.renderToFile("tile-1");
-  //   a.renderToFile("tile-2");
-
-  let alt = a.newRotated(2);
-  a.renderToFile("tile-1");
-  alt.renderToFile("tile-2");
-  console.log("RotEqal: " + a.checkRotationalEquality(t));
+  let n = 0;
+  lastList.forEach(tile => {
+    let s = "tile-" + n;
+    n += 1;
+    tile.renderToFile(s);
+  });
+  console.log("LENGTH: " + lastList.length);
 };
 
 module.exports = allTiles;
